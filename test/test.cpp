5741f63c738cf5d20503874e94416349785b88d9
makuke1234 <makuke1234@gmail.com>
2022-01-06 18:03:48 +0200

    Testing

---
+++ test/test.cpp
@@ -0,0 +1,30 @@
+#include "../include/xmlite.hpp"
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::ifstream input("test.xml");
+
+	std::stringstream iss;
+	iss << input.rdbuf();
+
+	input.close();
+
+	std::string str = iss.str();
+
+	std::cout << "File contents: \"" << str << "\"\n";
+
+	xmlite::xml xmlObject(str);
+
+	std::cout << "Version: " << xmlObject.getVersion() << '\n';
+	std::cout << "Encoding: " << xmlObject.getEncoding() << '\n';
+	std::cout << "Standalone: " << xmlObject.getStandalone() << '\n';
+
+	std::cout << "Reconstructed header: \"" << xmlObject.dumpHeader() << "\"\n";
+
+	return 0;
+}
